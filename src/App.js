7d730c876085571975e14f7e06ac76e61468e676
earthlingeve <ealanis1994@gmail.com>
2022-10-22 22:48:19 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Form from "./Form.js";
+import "./Form.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <strong>Weather App</strong>
         <Form />
       </header>
       <a href="https://github.com/earthlingeve/react-weather-app2">
