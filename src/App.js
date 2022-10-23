9ae3301bb77398c5fddd20d7e9613803c0e98fc3
earthlingeve <ealanis1994@gmail.com>
2022-10-22 22:12:56 -0500

    adjusted app.js to get the form to work

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
-        <h2>
-          <Form />
-        </h2>
+        <Form />
       </header>
     </div>
   );
