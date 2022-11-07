2b688c38abe542aa446f697fb1b382cd236a1dc0
earthlingeve <ealanis1994@gmail.com>
2022-11-07 14:38:14 -0600

    removed fahrenheit to celsius conversion

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,43 +1,10 @@
-import React, { useState } from "react";
-//import WeatherInfo from "./WeatherInfo.js";
+import React from "react";
 
 export default function WeatherTemp(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  if (unit === "fahrenheit") {
-    return (
-      <span className="WeatherTemp">
-        <span className="temperature">{Math.round(props.fahrenheit)}</span>
-        <span className="unit">
-          <a href="null">°F</a>|
-          <a href="null" onClick={showCelsius}>
-            °C
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    let celsius = ((props.fahrenheit - 32) * 5) / 9;
-    return (
-      <span className="WeatherTemp">
-        <span className="temperature">{Math.round(celsius)}</span>
-        <span className="unit">
-          <a href="null" onClick={showFahrenheit}>
-            °F
-          </a>
-          |<a href="null">°C</a>
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="WeatherTemp">
+      <span className="temperature">{Math.round(props.fahrenheit)}</span>
+      <span className="unit">°F</span>
+    </span>
+  );
 }
